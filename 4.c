522329ee5b955823884ce3cbab8619de56823473
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-01-08 10:54:18 +0000

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char m;
+printf("enter a character");
+scanf("%c",&m);
+if('z'||'x'||'c'||'v'||'b'||'n'||'m'||'a'||'s'||'d'||'f'||'g'||'h'||'j'||'k'||'l'||'q'||'w'||'e'||'r'||'t'||'y'||'u'||'i'||'o'||'p')';
+{
+printf("the character is alphabet");
+}
+else
+{
+printf("not");
+}
+}
