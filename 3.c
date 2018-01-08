eac12e8dd06400cce3ad52e2822ae66a7799fe33
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-01-08 10:43:27 +0000

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char m;
+printf("enter  the  character");
+scanf("%c",&m);
+if('a'||'e'||'i'||'o'||'u')
+{
+pritnf("the given character is vowel");
+}
+else
+{
+printf("the given character is odd");
+}
+}
