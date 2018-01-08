f420456b1c7e81337763755d09d2e742956cbd48
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-01-08 11:15:15 +0000

    Update 4.c

--- 4.c
+++ 4.c
@@ -4,7 +4,7 @@ void main()
 char m;
 printf("enter a character");
 scanf("%c",&m);
-if((m>'a'&&m<'z')||(m>'A'&&m<'Z'));
+if((m>='a'&&m<='z')||(m>='A'&&m<='Z'));
 {
 printf("the character is alphabet");
 }
