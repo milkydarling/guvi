dcc9bda10e7383615ef78c60e6f9f70f318ae1d5
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-01-08 11:24:17 +0000

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int year;
+printf("enter the  year");
+scanf("%d",&year);
+if(year%4==0)
+{
+if(year%100==0)
+{
+printf("the  given  year  is  leap");
+}
+else
+{
+printf("not  leap  year");
+}
+}
+else
+{
+printf("not leap  year");
+}
