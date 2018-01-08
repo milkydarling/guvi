29c8746bd146361a2b695ad772a8febd568e2f19
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-01-08 10:29:29 +0000

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a value);
+scanf("%d",&a);
+if(a<0)
+{
+printf("the given number is negative");
+}
+elseif(a>0)
+{
+printf("the given number is positive);
+}
+else
+{
+printf("the given number is zero");
+}
+}
