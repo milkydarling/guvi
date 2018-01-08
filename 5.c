0cfccef8170d54773945195a868dc0fa93454307
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-01-08 11:14:38 +0000

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter three values");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b)
+{
+if(a>c)
+{
+printf("a  is  big");
+}
+else
+{
+printf("c is big");
+}
+}
+else
+{
+printf("b is big");
+}
+}
