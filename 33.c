a835523f12399d0f12a5137f80da6d4f83de0a92
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-01-21 14:29:20 +0530

    Create 33.c

---
+++ 33.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+char str[100];
+int i,blank=0;
+printf("enter the string");
+scanf("%s",&str[i]);
+while(str[i]!='\0')
+{
+if(!((str[i]>='a')&&(str[i]<='z')))
+{
+blank=blank+1;
+}
+i++;
+}
+printf("%d",blank);
+}
