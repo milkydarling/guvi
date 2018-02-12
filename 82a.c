50ee63d77daee3b340ec93e282ce4da8738c592d
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-02-12 16:44:05 +0530

    Create 82a.c

---
+++ 82a.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int length,breadth;
+printf("enter length and breadth");
+scanf("%d  %d",&length,&breadth);
+double area=length*breadth;
+printf("the area of firm is %lf",area);
+}
