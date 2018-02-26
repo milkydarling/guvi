cb8436a28cea6d5c95a0204a067e3a8c773a2b14
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-02-26 15:57:52 +0530

    Create 93.c

---
+++ 93.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+int temp;
+float t;
+printf("enter the temperature in celcius");
+scanf("%d",&temp);
+t=temp+273.15;
+printf("the temperature in kelvin %f",t);
+}
