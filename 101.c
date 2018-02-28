70ca95fd73233711e1d94181c60faa0ba6e8d221
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-02-28 13:48:23 +0300

    Create 101.c

---
+++ 101.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char a[10];
+int n,i,x;
+scanf("%s",&a[i]);
+printf("enter number");
+scanf("%d",&n);
+x=strlen(a);
+/*for(i=0;a[i]!='\0';i++)
+{
+x++;
+}*/
+for(i=x-n;i<x;i++)
+{
+printf("%c",a[i]);
+}
+}
