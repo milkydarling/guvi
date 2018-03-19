17332a753fea4da87bdd7fef5a1310675c81abc0
milkydarling <31534904+milkydarling@users.noreply.github.com>
2018-03-19 16:04:41 +0530

    Create p20.c

---
+++ p20.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+        char s[10];
+        int i;
+        scanf("%s",&s[i]);
+        int l=strlen(s);
+        for(i=0;i<l;i++)
+        {
+                s[i]=s[i]+3;
+                printf("%c",s[i]);
+        }
+}
